cdb9675529f3dc2e22cee0aa58c469e2ad44b284
raggurun <ragu123@aol.in>
2021-09-30 16:09:27 +0530

    Update HelloFriends.java
    2nd commit

--- HelloFriends.java
+++ HelloFriends.java
@@ -5,5 +5,7 @@ public static void main(String[] args)
 
 System.out.println("Hi Everyone");
 System.out.println("Hi Learners");
+System.out.println("Hi Everyone");
+System.out.println("Hi Learners");
 }
 }
